1b472ef474bf58cd4ef9a3607597284fb4856ad9
Juan Murillo <jj.murillo182020@gmail.com>
2023-11-19 12:05:00 -0500

    cambiando loader image carousel

--- components/ui/Carousel.tsx
+++ components/ui/Carousel.tsx
@@ -1,9 +1,13 @@
 import {  useTheme } from "@mui/material";
 import useResponsive from "fleed/hooks/useResponsive";
-import Image from "next/image";
+import Image, { ImageLoader, ImageLoaderProps } from "next/image";
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 
+
+const contentfulImageLoader: ImageLoader = ({ src, width }: ImageLoaderProps) => {
+  return `${src}?w=${width}`
+}
 const CarouselComponent = () => {
   const theme = useTheme()
   const isPhone = useResponsive('down', 'md');
@@ -11,7 +15,11 @@ const CarouselComponent = () => {
   
   return (
     <Carousel  sx={{width:'100vw',position:'relative', height: {xs:'350px',  md : '500px', lg:'700px',xl :'1000px'}, marginTop : { xs : '80px',lg :"0"}}}>     
-        <Image  priority={false} src={ (theme.palette.mode === 'light') ? '/carousel/light.jpg' : '/carousel/dark.jpg'} alt="d" fill style={{ objectFit: (isPhone) ? 'contain' : 'cover'}} />
+        <Image 
+        fill
+        quality={100}
+        // loader={contentfulImageLoader}
+        sizes="100vw" priority={false} src={ (theme.palette.mode === 'light') ? '/carousel/light.jpg' : '/carousel/dark.jpg'} alt="Carousel Image"  style={{ objectFit: (isPhone) ? 'contain' : 'cover'}} />
     </Carousel>
   );
 };
