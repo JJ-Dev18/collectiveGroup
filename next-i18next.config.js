a8ec244b6f3c63a0592acab1f0d22e15b5ecb8ac
Juan Murillo <jj.murillo182020@gmail.com>
2023-11-28 13:26:38 -0500

    cambiando configuracion i18 con localePath

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next-i18next').UserConfig} */
+
+const path = require('path');
+
 module.exports = {
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'es'],
       localeDetection : false,
+      localePath: path.resolve('./public/locales')	,
     },
   }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
 const { i18n } = require('./next-i18next.config')
 const nextConfig = {
  
@@ -72,7 +71,7 @@ module.exports = {
     ]
   },
   ...nextConfig,
-  localePath: path.resolve('./public/locales')	,
+ 
   // reactStrictMode: false,
   // swcMinify: true,
   // onDemandEntries: {
